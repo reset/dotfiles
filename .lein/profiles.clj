c821939d4371f1dccece9d31a8e55bfec9c91170
Jamie Winsor <jamie@vialstudios.com>
2018-08-05 20:20:20 -0700

    Add leiningen profiles

---
+++ .lein/profiles.clj
@@ -0,0 +1,5 @@
+{:user
+ {:dependencies [
+                 [cljfmt "0.5.1"]
+                 [cider/cider-nrepl "0.17.0"]
+                 [jonase/kibit "0.1.6" :exclusions [org.clojure/clojure]]]}}
