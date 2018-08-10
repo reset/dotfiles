72c652ad7ad98803f25dfb165d31d1016ed96e61
Jamie Winsor <jamie@vialstudios.com>
2018-08-10 02:19:28 -0700

    Add eastwood plugin to lein profile
    Signed-off-by: Jamie Winsor <jamie@vialstudios.com>

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,5 +1,6 @@
 {:user
- {:dependencies [
-                 [cljfmt "0.5.1"]
-                 [cider/cider-nrepl "0.17.0"]
-                 [jonase/kibit "0.1.6" :exclusions [org.clojure/clojure]]]}}
+ {:dependencies [[cljfmt "0.5.1"]
+                 [cider/cider-nrepl "0.18.0"]
+                 [jonase/kibit "0.1.6" :exclusions [org.clojure/clojure]]
+                 [jonase/eastwood "0.2.9" :exclusions [org.clojure/clojure]]]
+  :plugins [[jonase/eastwood "0.2.9"]]}}
