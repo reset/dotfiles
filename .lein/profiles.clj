16ee0c34c9357fe037df7ef1f97592f26ccd7046
Jamie Winsor <jamie@vialstudios.com>
2018-08-14 10:34:50 -0700

    Add nrepl-refactor and cidr plugins to lein profile
    Signed-off-by: Jamie Winsor <jamie@vialstudios.com>

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,7 @@
 {:user
  {:dependencies [[cljfmt "0.5.1"]
-                 [cider/cider-nrepl "0.18.0"]
                  [jonase/kibit "0.1.6" :exclusions [org.clojure/clojure]]
                  [jonase/eastwood "0.2.9" :exclusions [org.clojure/clojure]]]
-  :plugins [[jonase/eastwood "0.2.9"]]}}
+  :plugins [[jonase/eastwood "0.2.9"]
+            [cidr/cider-nrepl "0.18.0"]
+            [refactor-nrepl "2.3.1"]]}}
