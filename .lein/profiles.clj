c5d361541dbbaefde298f52bb9036aba1cb7b8a2
Jamie Winsor <jamie@vialstudios.com>
2018-08-30 00:47:00 -0700

    Updated leiningen plugins
    Signed-off-by: Jamie Winsor <jamie@vialstudios.com>

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,7 +1,7 @@
 {:user
- {:dependencies [[cljfmt "0.5.1"]
-                 [jonase/kibit "0.1.6" :exclusions [org.clojure/clojure]]
-                 [jonase/eastwood "0.2.9" :exclusions [org.clojure/clojure]]]
-  :plugins [[jonase/eastwood "0.2.9"]
-            [cidr/cider-nrepl "0.18.0"]
-            [refactor-nrepl "2.3.1"]]}}
+ {:dependencies [[cljfmt "0.6.0"]]
+  :plugins [[lein-ancient "0.6.15"]
+            [cider/cider-nrepl "0.18.0"]
+            [jonase/kibit "0.1.6" :exclusions [org.clojure/clojure]]
+            [jonase/eastwood "0.2.9" :exclusions [org.clojure/clojure]]]
+}}
