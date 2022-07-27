ba6c09154bde776639e757a57ba654cc2eb2626b
Jamie Winsor <jamie@onemoregame.com>
2022-07-26 18:45:26 -0700

    build-essentials->build-essential
    Signed-off-by: Jamie Winsor <jamie@onemoregame.com>

--- bin/upgrade.sh
+++ bin/upgrade.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 sudo apt-get update &&
   sudo apt-get upgrade -y &&
   sudo apt-get install -y \
-    build-essentials \
+    build-essential \
     fonts-firacode \
     git-repair \
     gnutls-bin
